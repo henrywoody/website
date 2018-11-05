9c93aa0c69d99a8696fc99b4abca3b45459c01df
henrywoody <github@henrywoody.com>
2018-11-05 12:18:45 -0800

    updates Links page

--- client/src/pages/Links.vue
+++ client/src/pages/Links.vue
@@ -24,6 +24,41 @@ export default {
     data() {
         const sections = [
             {
+                title: "Web",
+                links: [
+                    {
+                        title: "Future Retro Redesigns",
+                        href: "https://interface.fh-potsdam.de/future-retro/"
+                    },
+                    {
+                        title: "7UP Netherlands",
+                        href: "https://www.7up.nl/7up"
+                    }
+                ]
+            },
+            {
+                title: "Artificial Life & Intelligence",
+                links: [
+                    {
+                        title: "Primordial Soup Kitchen",
+                        href: "http://psoup.math.wisc.edu/welcome.html"
+                    },
+                    {
+                        title: "Genetic Algorithms & Artificial Life YouTube Playlist",
+                        href: "https://www.youtube.com/playlist?list=PLhhW7H3Rz9W1Z59WwWwxEMofNyUc-0jBb"
+                    }
+                ]
+            },
+            {
+                title: "Math",
+                links: [
+                    {
+                        title: "Animated Factorization",
+                        href: "http://www.datapointed.net/visualizations/math/factorization/animated-diagrams/"
+                    }
+                ]
+            },
+            {
                 title: "Friends",
                 links: [
                     {
@@ -45,15 +80,16 @@ export default {
                     {
                         title: "Ethan Russell",
                         href: "http://ethan-russell.com"
+                    },
+                    {
+                        title: "Joe Marin",
+                        href: "https://joemarin.com/"
                     }
-                ]
+                ].sort((a, b) => {
+                    return a.title.split(" ")[1].charCodeAt(0) - b.title.split(" ")[1].charCodeAt(0);
+                })
             }
-        ].map(e => ({
-            ...e,
-            links: e.links.sort((a,b) => {
-                return a.title.split(" ")[1].charCodeAt(0) - b.title.split(" ")[1].charCodeAt(0)
-            })
-        }));
+        ]
         
         return { sections }
     }
