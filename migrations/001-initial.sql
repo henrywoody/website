a7639c01f50044dde3330f16928d079baffad56d
henrywoody <github@henrywoody.com>
2018-11-22 13:51:23 -0800

    makes use of environment variables in initial SQL migration

--- migrations/001-initial.sql
+++ migrations/001-initial.sql
@@ -1,5 +1,9 @@
-CREATE DATABASE website;
+\set db_name `echo $DB_NAME`
+\set db_user `echo $DB_USER`
+\set db_pass `echo $DB_PASS`
 
-CREATE ROLE webboi WITH LOGIN PASSWORD 'dbpassword1';
+CREATE DATABASE :db_name;
 
-GRANT ALL PRIVILEGES ON DATABASE website TO webboi;
+CREATE ROLE :db_user WITH LOGIN PASSWORD :'db_pass';
+
+GRANT ALL PRIVILEGES ON DATABASE :db_name TO :db_user;
