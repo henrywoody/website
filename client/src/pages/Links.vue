7c03ce721586e742187e1c85ecde0ad36a2ff2a6
henrywoody <github@henrywoody.com>
2020-09-13 19:57:40 -0700

    Adds Programming section to Links page
    Adds the Programming section to the Links page, which includes links to
    some helpful or entertaining programming sites.
    
    Also updates the way that the Links page is generated from using Vue
    templates for data saved in JSON format to just plain HTML.

--- client/src/pages/Links.vue
+++ client/src/pages/Links.vue
@@ -6,12 +6,104 @@
             Some great sites.
         </p>
 
-        <section v-for="section in sections" :key="section.title">
-            <h2>{{ section.title }}</h2>
+        <section>
+            <h2>Programming</h2>
 
             <ul>
-                <li v-for="link in section.links" :key="link.title">
-                    <ExternalLink :href="link.href">{{ link.title }}</ExternalLink>
+                <li>
+                    <ExternalLink href="https://www.codewars.com">Codewars</ExternalLink> (<ExternalLink href="https://www.codewars.com/users/henrywoody">my profile</ExternalLink>)
+                </li>
+
+                <li>
+                    <ExternalLink href="https://css-challenges.com">CSS Challenges</ExternalLink> (<ExternalLink href="https://css-challenges.com/members/henry">my profile</ExternalLink>)
+                </li>
+
+                <li>
+                    <ExternalLink href="https://github.com">GitHub</ExternalLink> (<ExternalLink href="https://github.com/henrywoody">my profile</ExternalLink>)
+                </li>
+
+                <li>
+                    <ExternalLink href="https://foobar.withgoogle.com">Google FooBar</ExternalLink>
+                </li>
+
+                <li>
+                    <ExternalLink href="http://www.pythonchallenge.com">Python Challenge</ExternalLink>
+                </li>
+
+                <li>
+                    <ExternalLink href="https://regexcrossword.com">Regex Crossword</ExternalLink> (<ExternalLink href="https://regexcrossword.com/profile/54291">my profile</ExternalLink>)
+                </li>
+
+                <li>
+                    <ExternalLink href="https://stackoverflow.com">Stack Overflow</ExternalLink> (<ExternalLink href="https://stackoverflow.com/users/8068625/henry-woody?tab=profile">my profile</ExternalLink>)
+                </li>
+            </ul>
+        </section>
+
+        <section>
+            <h2>Web</h2>
+
+            <ul>
+                <li>
+                    <ExternalLink href="https://interface.fh-potsdam.de/future-retro">Future Retro Redesigns</ExternalLink>
+                </li>
+
+                <li>
+                    <ExternalLink href="https://www.7up.nl/7up">7UP Netherlands</ExternalLink>
+                </li>
+            </ul>
+        </section>
+
+        <section>
+            <h2>Artificial Life &amp; Intelligence</h2>
+
+            <ul>
+                <li>
+                    <ExternalLink href="http://psoup.math.wisc.edu/welcome.html">Primordial Soup Kitchen</ExternalLink>
+                </li>
+
+                <li>
+                    <ExternalLink href="https://www.youtube.com/playlist?list=PLhhW7H3Rz9W1Z59WwWwxEMofNyUc-0jBb">Genetic Algorithms & Artificial Life YouTube Playlist</ExternalLink>
+                </li>
+            </ul>
+        </section>
+
+        <section>
+            <h2>Math</h2>
+
+            <ul>
+                <li>
+                    <ExternalLink href="http://www.datapointed.net/visualizations/math/factorization/animated-diagrams">Animated Factorization</ExternalLink>
+                </li>
+            </ul>
+        </section>
+
+        <section>
+            <h2>Friends</h2>
+
+            <ul>
+                <li>
+                    <ExternalLink href="https://andrewlangtry.com">Andrew Langtry</ExternalLink>
+                </li>
+
+                <li>
+                    <ExternalLink href="https://www.verychill.biz">Jesse Jenks</ExternalLink>
+                </li>
+
+                <li>
+                    <ExternalLink href="http://banj.io">Banji Oyewole</ExternalLink>
+                </li>
+
+                <li>
+                    <ExternalLink href="https://chilij.com">Chili Johnson</ExternalLink>
+                </li>
+
+                <li>
+                    <ExternalLink href="http://ethan-russell.com">Ethan Russell</ExternalLink>
+                </li>
+
+                <li>
+                    <ExternalLink href="https://joemarin.com/">Joe Marin</ExternalLink>
                 </li>
             </ul>
         </section>
@@ -26,77 +118,5 @@ export default {
     components: {
         ExternalLink,
     },
-    data() {
-        const sections = [
-            {
-                title: "Web",
-                links: [
-                    {
-                        title: "Future Retro Redesigns",
-                        href: "https://interface.fh-potsdam.de/future-retro/"
-                    },
-                    {
-                        title: "7UP Netherlands",
-                        href: "https://www.7up.nl/7up"
-                    }
-                ]
-            },
-            {
-                title: "Artificial Life & Intelligence",
-                links: [
-                    {
-                        title: "Primordial Soup Kitchen",
-                        href: "http://psoup.math.wisc.edu/welcome.html"
-                    },
-                    {
-                        title: "Genetic Algorithms & Artificial Life YouTube Playlist",
-                        href: "https://www.youtube.com/playlist?list=PLhhW7H3Rz9W1Z59WwWwxEMofNyUc-0jBb"
-                    }
-                ]
-            },
-            {
-                title: "Math",
-                links: [
-                    {
-                        title: "Animated Factorization",
-                        href: "http://www.datapointed.net/visualizations/math/factorization/animated-diagrams/"
-                    }
-                ]
-            },
-            {
-                title: "Friends",
-                links: [
-                    {
-                        title: "Andrew Langtry",
-                        href: "https://andrewlangtry.com"
-                    },
-                    {
-                        title: "Jesse Jenks",
-                        href: "https://www.verychill.biz"
-                    },
-                    {
-                        title: "Banji Oyewole",
-                        href: "http://banj.io"
-                    },
-                    {
-                        title: "Chili Johnson",
-                        href: "https://chilij.com"
-                    },
-                    {
-                        title: "Ethan Russell",
-                        href: "http://ethan-russell.com"
-                    },
-                    {
-                        title: "Joe Marin",
-                        href: "https://joemarin.com/"
-                    }
-                ].sort((a, b) => {
-                    return a.title.split(" ")[1].charCodeAt(0) - b.title.split(" ")[1].charCodeAt(0);
-                })
-            }
-        ]
-
-        return { sections }
-    }
 }
 </script>
